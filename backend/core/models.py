b778840a8665a1553316ebab04aa8e427c1fbd3a
Jonathan de Jesus Obregón <jonathanobregon2409@ca.frre.utn.edu.ar>
2026-04-21 17:21:56 -0300

    add models: Zona, Cliente, Vendedor + config MySQL y .env

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Zona
+class Zona(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.nombre
+    
+
+# Cliente
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    nro_documento = models.CharField(max_length=50, unique=True)
+    telefono = models.CharField(max_length=50)
+    email = models.EmailField()
+    direccion = models.CharField(max_length=150)
+    localidad = models.CharField(max_length=100)
+    provincia = models.CharField(max_length=100)
+
+    usuario = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True)
+    zona = models.ForeignKey(Zona, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.nombre
+    
+
+# Vendedor
+class Vendedor(models.Model):
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=50)
+    comision = models.DecimalField(max_digits=5, decimal_places=2)
+    estado = models.CharField(max_length=50)
+
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    zona = models.ForeignKey(Zona, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.nombre
